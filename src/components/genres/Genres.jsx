d3326146e5a38347bce6bf20d4ee10b4e863271a
Hrushiingole <hrishikeshingole532@gmail.com>
2024-02-26 22:27:13 +0530

    some changes for ui

--- src/components/genres/Genres.jsx
+++ src/components/genres/Genres.jsx
@@ -4,20 +4,15 @@ import {  useSelector } from 'react-redux'
 
 export default function Genres({data}) {
     const {genres}=useSelector((state)=>state.home)
-    console.log(data)
-    // console.log(data);
-    // console.log(typeof(data));
-    // let datalist=Object.keys(data).map(key=>{
-    //     return [data[key]];
-    // });
-    // console.log(datalist);
+    // console.log(data)
+    
     
   return (
     <div className="genres">
 
         {data?.map((g)=>{
             
-            console.log(genres[g].name);
+            // console.log(genres[g].name);
             if(!genres[g]?.name) return;
             return (
                 <div key={g} className="genre">
